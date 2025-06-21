a7e364b90d8956a12de3ea161c862ea44c5aba52
Ayushman Koley <80102808+ayushmankoley@users.noreply.github.com>
2025-06-21 12:27:24 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -120,7 +120,7 @@ function AppContent() {
             <div className="md:col-span-2">
               <div className="flex items-center space-x-2 mb-4">
                 <img 
-                  src="src\images\landscape_logo_ts2.png" 
+                  src="..\images\landscape_logo_ts2.png" 
                   alt="GrowMint"
                   className="h-25 w-60 object-contain"
                 />
